a971fd25309490304a35822acf82aa08092e73bc
Mehmet <mhmtonar1@gmail.com>
2018-06-10 17:08:25 +0300

    iki tarih arasındaki filmlerin sorgulanma url oluşturuldu

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ const db = require("./helper/db")();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,20 +39,21 @@ app.use('/api/movies', movieRouter);
  */
 
 // catch 404 and forward to error handler
-app.use((req, res, next)=> {
-  next(createError(404));
+app.use((req, res, next) => {
+    next(createError(404));
 });
 
 
 // error handler
-app.use((err, req, res, next) =>{
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({error: {message: err.message, code: err.code}});//next ile gönderdiğin obje ile tam uyuşacak
 });
 
 module.exports = app;
--- routes/movie.js
+++ routes/movie.js
@@ -25,18 +25,43 @@ router.get("/", (req, res) => {
 /**
  * end get all movies
  */
-router.get("/:movie_id",(req,res)=>{
+/**
+ * top 10 list
+ */
+router.get("/top10", (req, res) => {
+
+    const allMovies = MovieSchema.find({}).sort({imdb_score : -1}).limit(10);
+    allMovies.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        console.log(__dirname,"~/api-movie/ url hatası :", err)
+    })
+
+});
+/**
+ * end top 10 list
+ */
+/**
+ * get movie with by id
+ */
+router.get("/:movie_id",(req,res,next)=>{
     //req.params => :movie_id
     const movieById = MovieSchema.findById(req.params.movie_id);
     movieById.then((movie)=>{
+        if (!movie){
+            next({message :"data dönmedi", code:1});
+        }
+
         res.json(movie);
     }).catch((err)=>{
-        res.json("/api/movies/:movie_id =>",err);
+        res.json(err);
         console.log("/api/movies/:movie_id =>",err);
     });
 
 });
-
+/**
+ * end get movie with by id
+ */
 /**
  * save data
  */
@@ -67,4 +92,78 @@ router.post('/', (req, res, next) =>{
  * end save data
  */
 
+/**
+ * update data using id
+ */
+router.put("/:movie_id",(req,res,next)=>{
+
+    const movieSchema = MovieSchema.findByIdAndUpdate(req.params.movie_id,req.body,{new:true});
+    movieSchema.then((updatedMovie)=>{
+        if (!updatedMovie){
+            next({message:"güncelleme hatası", code:2});
+        }
+        res.json(updatedMovie);
+    }).catch((err)=>{
+        console.log(_dirname,"movie.js => update error drom movie.js ",err);
+        res.json(err);
+    });
+
+});
+/**
+ * update data using id
+ */
+
+
+/**
+ * delete movie using movie id
+ */
+router.delete("/:movie_id",(req,res,next)=>{
+    //this.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+    const movieSchema = MovieSchema.findByIdAndRemove(req.params.movie_id);
+    movieSchema.then((deletedMovie)=>{
+        if (!deletedMovie)
+            setTimeout(()=>{
+                next({message:"silme hatası", code:3})
+            },1000);
+        res.json({deletedMovie:{deletedMovie}});
+    }).catch((err)=>{
+        console.log(__dirname,"movie.js => error while deleting =>",err)
+    });
+});
+/**
+ * delete movie using movie id
+ */
+
+/**
+ * between two years
+ */
+router.get("/between/:start_year/:end_year", (req, res) => {
+    const {start_year,end_year} = req.params;
+    const allMovies = MovieSchema.find(
+        {
+            year :
+            {
+                "$gte":parseInt(start_year), "$lte": parseInt(end_year)
+                //gt yada lt kullanırsan sınırları almaz
+            }
+        });
+    allMovies.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        console.log(__dirname,"~/api-movie/ iki yıl arasındali sorgu hatası :", err)
+    })
+
+});
+/**
+ * between two years
+ */
+
+
+
+/**
+ *
+ */
+
+//
 module.exports = router;
